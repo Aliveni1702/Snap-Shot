5ffb4722d21d3dc845cbd94d435dddbd2ea0f026
Aliveni1702 <alivenimangalapati@gmail.com>
2023-02-28 13:30:58 +0530

    snapshot assignment added

---
+++ src/gallery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./gallery.css"
+const Gallery = ({data}) =>{
+    return(
+        <div>
+            <div className='grid'>
+                {data.map((image)=><div key={image.id}>
+                    <div className='col-md-4'>
+                        <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`} height="200" width="250" alt='images'/>
+                        </div>
+                </div>)}
+            </div>
+            
+        </div>
+    )
+}
+ export default Gallery;
